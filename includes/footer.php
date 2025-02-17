41a6ff312a503175ddb6602c2d473fee4490a930
chinnarat07 <bu.chinnarat@gmail.com>
2025-02-17 10:51:18 +0700

    up

---
+++ includes/footer.php
@@ -0,0 +1,142 @@
+ <!-- footer start -->
+ <footer id="footer" class="overflow-hidden padding-large">
+    <div class="container">
+      <div class="row d-flex flex-wrap justify-content-between">
+        <div class="col-lg-3 col-md-6 col-sm-6 pb-3">
+          <div class="footer-menu ">
+            <img src="images/logo-文字版.png" style="height:100px; width:100px; object-fit:cover;" alt="footer logo" class="pb-2 mx-auto d-block"/>
+            <p class="fw-semibold text-center" style="font-size: 1rem;">บริการรับเหมาก่อสร้าง <br> บริษัท เอ.เอ็กซ์.ดับบลิว อินเตอร์เนชันแนล จํากัด <br> A.X.W INTERNATIONAL CO.,LTD.</p>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-6 pb-3">
+          <div class="footer-menu">
+            <ul class="menu-list list-unstyled">
+              <li class="pb-2 fw-bold text-capitalize">
+                <a href="#about">Home</a>
+              </li>
+              <li class="pb-2 fw-bold text-capitalize">
+                <a href="#">Services</a>
+              </li>
+              <li class="pb-2 fw-bold text-capitalize">
+                <a href="#">News</a>
+              </li>
+              <li class="pb-2 fw-bold text-capitalize">
+                <a href="#">Terms of use</a>
+              </li>
+              <li class="pb-2 fw-bold text-capitalize">
+                <a href="#">Privacy Policy</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 pb-3">
+          <div class="footer-menu">
+            <ul class="menu-list list-unstyled">
+              <li class="pb-2 fw-bold text-capitalize">
+                <a href="#about">Our location</a>
+              </li>
+              <li class="pb-2 fw-bold text-capitalize">
+                <a href="#">About </a>
+              </li>
+              <li class="pb-2 fw-bold text-capitalize">
+                <a href="#">Contact </a>
+              </li>
+              <li class="pb-2 fw-bold text-capitalize">
+                <a href="#">Help Center</a>
+              </li>
+              <li class="pb-2 fw-bold text-capitalize">
+                <a href="#">FAQs</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 pb-3">
+          <div class="footer-menu">
+            <ul class="menu-list list-unstyled">
+              <li class="pb-2 fw-bold text-capitalize">
+                <svg class="text-primary" width="25" height="20" aria-hidden="true">
+                  <use xlink:href="#location"></use>
+                </svg>
+                163 Elm Drive, New York City
+              </li>
+              <li class="pb-2 fw-bold text-capitalize">
+                <svg class="text-primary" width="25" height="20" aria-hidden="true">
+                  <use xlink:href="#call"></use>
+                </svg>(+089) 234-516-6123
+              </li>
+              <li class="pb-2 fw-bold text-capitalize">
+                <svg class="text-primary" width="25" height="20" aria-hidden="true">
+                  <use xlink:href="#mail"></use>
+                </svg>
+                <a href="mailto:">info@yourinfo.com</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- footer end -->
+
+  <div class="footer-bottom d-flex flex-wrap justify-content-between border-top pt-5 pb-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <p>
+            ©2024 Bamboo | All Rights Reserved | Template by <strong>
+              <a href="https://templatesjungle.com/" target="_blank">TemplatesJungle</a>
+            </strong>
+          </p>
+        </div>
+        <div class="col-lg-6">
+          <ul class="social-links list-unstyled d-flex justify-content-lg-end">
+            <li class="pe-3">
+              <a href="#">
+                <svg class="facebook text-primary-500" width="15" height="15" aria-hidden="true">
+                  <use xlink:href="#facebook"></use>
+                </svg>
+              </a>
+            </li>
+            <li class="pe-3">
+              <a href="#">
+                <svg class="twitter text-primary-500" width="15" height="15" aria-hidden="true">
+                  <use xlink:href="#twitter"></use>
+                </svg>
+              </a>
+            </li>
+            <li class="pe-3">
+              <a href="#">
+                <svg class="instagram text-primary-500" width="15" height="15" aria-hidden="true">
+                  <use xlink:href="#instagram"></use>
+                </svg>
+              </a>
+            </li>
+            <li class="pe-3">
+              <a href="#">
+                <svg class="linkedin text-primary-500" width="15" height="15" aria-hidden="true">
+                  <use xlink:href="#linkedin"></use>
+                </svg>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <svg class="youtube text-primary-500" width="15" height="15" aria-hidden="true">
+                  <use xlink:href="#blogger"></use>
+                </svg>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="js/jquery-1.11.0.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+  <script type="text/javascript" src="aos/aos.js"></script>
+  <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+  <script type="text/javascript" src="js/plugins.js"></script>
+  <script type="text/javascript" src="js/script.js"></script>
+  <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+</body>
+
+</html>
