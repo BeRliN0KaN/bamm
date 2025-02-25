14e4de279adbf366ec6e8e42497f5970297579cc
KAN_J <thegoathjclan@gmail.com>
2025-02-25 10:02:33 +0700

    up

--- 44_ITservice_dd.php
+++ 44_ITservice_dd.php
@@ -6,7 +6,7 @@
 <!-- Feature Start -->
 <div class="container-fluid feature overflow-hidden py-5">
     <div class="container py-5">
-
+        
         <div class="row g-5" style="margin-top: 3rem;">
             <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 900px;">
                 <h1 class="display-5 mb-4"><?php echo constant('page_itsupport_10') ?></h1>
@@ -18,8 +18,8 @@
                     <div class="text-center p-4">
                         <div class="d-inline-flex align-items-center justify-content-center bg-primary-gradient rounded-circle mb-4" style="width: 100px; height: 100px;"><i class="bi bi-router fs-1"></i></div>
                         <div class="feature-content">
-                            <a href="#" class="h4">GSM Gateway System</a>
-                            <p class="mt-4 mb-0"><?php echo constant('page_itsupport_3') ?></p>
+                            <a href="#" class="h4"><?php echo constant('page_itsupport_3') ?></a>
+                            <p class="mt-4 mb-0"><?php echo constant('page_itsupport_4') ?></p>
                         </div>
                     </div>
                 </div>
@@ -28,8 +28,8 @@
                     <div class="text-center p-4">
                         <div class="d-inline-flex align-items-center justify-content-center bg-primary-gradient rounded-circle mb-4" style="width: 100px; height: 100px;"><i class="bi bi-phone-vibrate fs-1"></i></div>
                         <div class="feature-content">
-                            <a href="#" class="h4">VOIP System</a>
-                            <p class="mt-4 mb-0"><?php echo constant('page_itsupport_4') ?></p>
+                            <a href="#" class="h4"><?php echo constant('page_itsupport_5') ?></a>
+                            <p class="mt-4 mb-0"><?php echo constant('page_itsupport_6') ?></p>
                         </div>
                     </div>
                 </div>
@@ -38,58 +38,17 @@
                     <div class="text-center rounded p-4">
                         <div class="d-inline-flex align-items-center justify-content-center bg-primary-gradient rounded-circle mb-4" style="width: 100px; height: 100px;"><i class="bi bi-pci-card-network fs-1"></i></div>
                         <div class="feature-content">
-                            <a href="#" class="h4">IPBX</a>
-                            <p class="mt-4 mb-0"><?php echo constant('page_itsupport_5') ?></p>
+                            <a href="#" class="h4"><?php echo constant('page_itsupport_7') ?></a>
+                            <p class="mt-4 mb-0"><?php echo constant('page_itsupport_8') ?></p>
                         </div>
                     </div>
-                </div>
-                <!-- Conference System -->
-                <div class="col-md-6 col-lg-4 col-xl-3 wow fadeInUp" data-wow-delay="0.7s">
-                    <div class="text-center rounded p-4">
-                        <div class="d-inline-flex align-items-center justify-content-center bg-primary-gradient rounded-circle mb-4" style="width: 100px; height: 100px;"><i class="bi bi-usb-micro fs-1"></i></div>
-                        <div class="feature-content">
-                            <a href="#" class="h4">Conference System</a>
-                            <p class="mt-4 mb-0"><?php echo constant('page_itsupport_6') ?></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="row g-4 justify-content-center text-center mb-5">
-                    <!-- GSM Gateway System -->
-                    <div class="col-md-6 col-lg-4 col-xl-3 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="text-center p-4">
-                            <div class="d-inline-flex align-items-center justify-content-center bg-primary-gradient rounded-circle mb-4" style="width: 100px; height: 100px;"><i class="bi bi-headset fs-1"></i></div>
-                            <div class="feature-content">
-                                <a href="#" class="h4">Voice Logger</a>
-                                <p class="mt-4 mb-0"><?php echo constant('page_itsupport_7') ?></p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Voice Logger -->
-                    <div class="col-md-6 col-lg-4 col-xl-3 wow fadeInUp" data-wow-delay="0.3s">
-                        <div class="text-center p-4">
-                            <div class="d-inline-flex align-items-center justify-content-center bg-primary-gradient rounded-circle mb-4" style="width: 100px; height: 100px;"><i class="bi bi-code-square fs-1"></i></div>
-
-                            <div class="feature-content">
-                                <a href="#" class="h4">ELASTIX</a>
-                                <p class="mt-4 mb-0"><?php echo constant('page_itsupport_8') ?></p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- ELASTIX -->
-                    <div class="col-md-6 col-lg-4 col-xl-3 wow fadeInUp" data-wow-delay="0.5s">
-                        <div class="text-center p-4">
-                            <div class="d-inline-flex align-items-center justify-content-center bg-primary-gradient rounded-circle mb-4" style="width: 100px; height: 100px;"><i class="bi bi-wifi fs-1"></i></div>
-                            <div class="feature-content">
-                                <a href="#" class="h4">Radio over Internet</a>
-                                <p class="mt-4 mb-0"><?php echo constant('page_itsupport_9') ?></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                </div
+                >
             </div>
         </div>
     </div>
 </div>
+
 <!-- Feature End -->
 
 <!-- footer start -->
